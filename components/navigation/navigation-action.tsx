f2a1036647e1a78413b65c5f436813f8d7315e23
Puskar-Roy <puskarroy600@gmail.com>
2024-01-30 19:27:13 +0530

    feat : server loading

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -7,8 +7,8 @@ const NavigationAction = () => {
     <div>
       <Actiontooltip side="right" align="center" label="Add a Playground">
       <button className="group flex items-center">
-        <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-backgroundcdark:bg-neutral-700 group-hover:bg-rose-500">
-      <Plus className="group-hover:text-whitw transition tsxt-rose-500" size={25}/>
+        <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-rose-500">
+      <Plus className="group-hover:text-whitw transition group-hover:text-rose-200" size={25}/>
         </div>
       </button>
       </Actiontooltip>
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { currentProfile } from '../../lib/current-profile';
-import { redirect } from 'next/navigation';
-import { db } from '../../lib/db';
-import NavigationAction from './navigation-action'
+import React from "react";
+import { currentProfile } from "../../lib/current-profile";
+import { redirect } from "next/navigation";
+import { db } from "../../lib/db";
+import NavigationAction from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "..//ui/scroll-area";
+import Navigationitem from "./navigation-item";
 
 const Navigationsidebar = async () => {
-    const profile = await currentProfile();
-    if(!profile){
-        return redirect("/");
-    }
-    const server = await db.server.findMany({
-        where:{
-            members:{
-                some : {
-                    profileId : profile.id
-                }
-            }
-        }
-    })
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirect("/");
+  }
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  console.log(servers);
+
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-      <NavigationAction/>
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full ">
+        {servers.map((s) => (
+          <div className="mb-4" key={s.id}>
+            <Navigationitem id={s.id} name={s.name} imageUrl={s.imageUrl} />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
-  )
-}
+  );
+};
 
-export default Navigationsidebar
+export default Navigationsidebar;
